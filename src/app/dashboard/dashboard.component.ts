9ab8ce4175471047b96e2ed2a7fc09ee1ac0aaa4
darshangit <darshans1989@gmail.com>
2017-06-27 20:45:38 -0700

    dynamic carousal and onclick functionbality.Looks awesome now.

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,24 +1,29 @@
 import { Component, OnInit, Inject, NgModule } from '@angular/core'
 import { MainTopicsModel } from 'app/data-models/main-topics.model'
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
 import { CarousalResponse } from 'app/data-models/carousal-response.model'
 import { JQ_TOKEN } from '../common/jQuery.services'
 
 @Component({
     selector: 'app-dashboard',
     templateUrl: './dashboard.component.html',
-    styleUrls:['../css/topics.less']
+    styleUrls: ['../css/topics.less']
 })
-export class DashboardComponent implements OnInit{
+export class DashboardComponent implements OnInit {
     topicsArray: MainTopicsModel[]
     carousalList: CarousalResponse[]
-
-    constructor(@Inject(JQ_TOKEN) private $: any, private activatedRoute: ActivatedRoute) {}
+    constructor( @Inject(JQ_TOKEN) private $: any, private activatedRoute: ActivatedRoute, private route: Router) { }
 
     ngOnInit() {
         this.topicsArray = this.activatedRoute.snapshot.data['topics']
         this.carousalList = this.activatedRoute.snapshot.data['carousal']
 
-        setTimeout(() => { this.$('#theCarousal').carousel() }, 1000)
+        this.$('#theCarousal').carousel()
+    }
+
+    openListing(carousal: CarousalResponse) {
+        if (carousal.carousalType === 'Topic') {
+            this.route.navigate(['/subListings', carousal.carousalName, carousal.carousalLink])
+        }
     }
 }
